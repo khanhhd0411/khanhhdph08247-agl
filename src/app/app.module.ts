e58608269a1252c69f38172492d7e964dabcc9de
khanhhd0411 <kienkakax1@gmail.com>
2020-03-25 08:58:39 +0700

    9 files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ProductService } from './services/product.service';
+import { SlideComponent } from './slide/slide.component';
+import { CategoryComponent } from './category/category.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AboutComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, AboutComponent, HomeComponent, SlideComponent, CategoryComponent, NotFoundComponent, ContactComponent, AddProductComponent, ProductComponent, ProductDetailComponent, ProductListComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ProductService]
 })
 export class AppModule { }
